cfea43a0b3dfdb9f328cb7c029f659143010e777
Brian McKenna <brian@brianmckenna.org>
2018-04-02 07:14:40 -0700

    Add version of Nix which includes setOptions patch

--- overlay.nix
+++ overlay.nix
@@ -8,4 +8,15 @@ self: super: {
   vpnkit = self.callPackage ./vpnkit { };
   linuxkit = self.callPackage ./linuxkit { };
   linuxkit-builder = self.callPackage ./linuxkit-builder { };
+
+  nixUnstable = self.nix.overrideDerivation (drv: {
+    name = "nix-2.0pre6018_g088ef817";
+    src = self.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nix";
+      rev = "088ef81759f22bf0115a52f183ba66b0be3b9ef2";
+      sha256 = "1rj20lllf9awx0150frxckgwv1h6a1rv90dyz206lp3b4jvsf7pf";
+    };
+    nativeBuildInputs = drv.nativeBuildInputs ++ (with self; [ autoreconfHook autoconf-archive bison flex libxml2 libxslt docbook5 docbook5_xsl ]);
+  });
 }
