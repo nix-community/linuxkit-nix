4e6316625ac8e7b8046c1cf23d7c9852f7b16bbb
zimbatm <zimbatm@zimbatm.com>
2018-11-07 16:47:50 +0100

    overlay: use super instead of self
    Rename "super" to "pkgs" as this is the default-one to use.
    
    This change allows for further overrides of the overlay.

--- overlay.nix
+++ overlay.nix
@@ -1,19 +1,19 @@
-self: super: {
-  pkgsForLinux = import self.path {
+self: pkgs: {
+  pkgsForLinux = import pkgs.path {
     system = "x86_64-linux";
     overlays = [ (import ./overlay.nix) ];
   };
 
-  hyperkit = self.callPackage ./hyperkit {
-    inherit (self.darwin.apple_sdk.frameworks) Hypervisor vmnet;
-    inherit (self.darwin.apple_sdk.libs) xpc;
-    inherit (self.darwin) libobjc;
+  hyperkit = pkgs.callPackage ./hyperkit {
+    inherit (pkgs.darwin.apple_sdk.frameworks) Hypervisor vmnet;
+    inherit (pkgs.darwin.apple_sdk.libs) xpc;
+    inherit (pkgs.darwin) libobjc;
   };
-  virtsock = self.callPackage ./virtsock { };
-  vpnkit = self.callPackage ./vpnkit { };
-  go-vpnkit = self.callPackage ./go-vpnkit { };
-  linuxkit = self.callPackage ./linuxkit { };
-  linuxkit-builder = self.callPackage ./linuxkit-builder { };
-  nix-linuxkit-runner = (self.callPackage ./nix-linuxkit-runner/Cargo.nix { }).nix_linuxkit_runner {};
-  nix-script-store-plugin = self.callPackage ./nix-script-store-plugin { };
+  virtsock = pkgs.callPackage ./virtsock { };
+  vpnkit = pkgs.callPackage ./vpnkit { };
+  go-vpnkit = pkgs.callPackage ./go-vpnkit { };
+  linuxkit = pkgs.callPackage ./linuxkit { };
+  linuxkit-builder = pkgs.callPackage ./linuxkit-builder { };
+  nix-linuxkit-runner = (pkgs.callPackage ./nix-linuxkit-runner/Cargo.nix { }).nix_linuxkit_runner {};
+  nix-script-store-plugin = pkgs.callPackage ./nix-script-store-plugin { };
 }
