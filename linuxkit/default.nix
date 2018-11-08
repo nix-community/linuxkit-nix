5d6ee1906a73c02aba2a3af9724a4722d092f264
zimbatm <zimbatm@zimbatm.com>
2018-11-08 08:44:18 +0100

    linuxkit: 0.2 -> 0.6

--- linuxkit/default.nix
+++ linuxkit/default.nix
@@ -1,8 +1,12 @@
 { lib, buildGoPackage, go, fetchFromGitHub }:
 
-buildGoPackage rec {
+let
+  # Make sure to keep those in sync
+  version = "0.6";
+  rev = "10f07ca";
+in
+buildGoPackage {
   name = "linuxkit-${version}";
-  version = "0.2";
 
   goPackagePath = "github.com/linuxkit/linuxkit";
 
@@ -10,13 +14,13 @@ buildGoPackage rec {
     owner = "linuxkit";
     repo = "linuxkit";
     rev = "v${version}";
-    sha256 = "1y7pjmzimnm52v218fznqg8gjiwzxg38ywxiqig8iiljpc6hiyha";
+    sha256 = "12nph1sxgp7l2sb3ar7x8a2rrk2bqphca6snwbcqaqln2ixsh78i";
   };
 
   subPackages = [ "src/cmd/linuxkit" ];
 
   preBuild = ''
-    buildFlagsArray+=("-ldflags" "-X main.GitCommit=1c552f7 -X main.Version=0.2.0")
+    buildFlagsArray+=("-ldflags" "-X main.GitCommit=${rev} -X main.Version=${version}")
   '';
 
   meta = {
